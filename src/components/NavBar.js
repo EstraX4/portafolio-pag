68fe016acb020910c5a146e68b367a160395206a
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2022-12-01 17:48:45 -0600

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,6 +16,11 @@ useEffect(() => {
     window.addEventListener('scroll', onScroll);
     return () => window.removeEventListener('scroll', onScroll);
 }, [])
+
+const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+}
+
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
@@ -27,9 +32,9 @@ useEffect(() => {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#proyects">Proyects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')} >Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')} >Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} >Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
@@ -37,7 +42,7 @@ useEffect(() => {
                 <a href="#"><img src={} alt="" /></a>
                 <a href="#"><img src={} alt="" /></a>
                 </div>
-                <button className="vvd" onClick={() => console.log('conect')}><span>Let´s Connect</span></button>
+                <button className="vvd" onClick={() => console.log('conect')}><span>Let's Connect</span></button>
           </span>
         </Navbar.Collapse>
       </Container>
