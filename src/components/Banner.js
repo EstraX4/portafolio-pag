2dd03996ded6245688fc30e7c50e83348fafcb77
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2022-12-03 15:49:03 -0600

    update

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import { Container, Row } from "react-bootstrap";
+
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <container>
+        <Row className="align-items-center">
+          <col></col>
+        </Row>
+      </container>
+    </section>
+  );
+};
