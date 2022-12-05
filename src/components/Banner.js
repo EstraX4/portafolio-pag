d09cf72c5fb27b14880316788692c319e792ebec
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2022-12-04 19:15:12 -0600

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,10 @@ export const Banner = () => {
     <section className="banner" id="home">
       <container>
         <Row className="align-items-center">
-          <col></col>
+          <col xs={12} md={6} xl={7}>
+            <span className="tag-line">Welcome to my Portafolio></span>
+            <h1>{}</h1>
+          </col>
         </Row>
       </container>
     </section>
