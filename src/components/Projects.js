ced7e4562994f2985c28da2801a1db0d6eb83e34
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2022-12-06 21:08:49 -0600

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
+import CertImg1 from "../assets/img/certif-img1.png";
 import estImg1 from "../assets/img/estudi-img1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
@@ -23,9 +24,9 @@ export const Projects = () => {
 
   const certificates = [
     {
-      title: "Certificate",
-      description: "Certificate Description",
-      imgUrl: projImg2,
+      title: "Responsive Web Design",
+      description: "freeCodeCamp",
+      imgUrl: CertImg1,
     },
   ];
 
