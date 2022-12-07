75689e091f2989ec6de080de92e5555eec1367ee
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2022-12-06 21:02:26 -0600

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,6 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
 import estImg1 from "../assets/img/estudi-img1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
@@ -11,63 +10,18 @@ import TrackVisibility from "react-on-screen";
 export const Projects = () => {
   const projects = [
     {
-      title: "Projects",
-      description: "Projects Description",
+      title: "Pastry page",
+      description: "Page made with html and css.",
       imgUrl: projImg1,
     },
     {
-      title: "Projects",
-      description: "Projects Description",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Projects",
-      description: "Projects Description",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Projects",
-      description: "Projects Description",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Projects",
-      description: "Projects Description",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Projects",
-      description: "Projects Description",
-      imgUrl: projImg1,
+      title: "Vaccination page",
+      description: "Page made with html and css.",
+      imgUrl: projImg2,
     },
   ];
 
   const certificates = [
-    {
-      title: "Certificate",
-      description: "Certificate Description",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Certificate",
-      description: "Certificate Description",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Certificate",
-      description: "Certificate Description",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Certificate",
-      description: "Certificate Description",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Certificate",
-      description: "Certificate Description",
-      imgUrl: projImg2,
-    },
     {
       title: "Certificate",
       description: "Certificate Description",
