0677f487bbfd31deb337af8572c0ac127121e007
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2022-12-30 14:56:25 -0600

    Skill Upgrades

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,75 +1,239 @@
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 5,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <br></br>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>HTML</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>CSS</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>JavaScript</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>React</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Node.js</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Mongodb</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Git</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <br></br>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="owl-carousel owl-theme skill-slider"
+              >
+                <div className="item">
+                  <a
+                    href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+                      alt="html5"
+                      width="40"
+                      height="40"
+                    />{" "}
+                  </a>
                 </div>
+                <div className="item">
+                  <a
+                    href="https://www.w3schools.com/css/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+                      alt="css3"
+                      width="40"
+                      height="40"
+                    />{" "}
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://developer.mozilla.org/es/docs/Web/JavaScript"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                      alt="javascript"
+                      width="40"
+                      height="40"
+                    />{" "}
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://getbootstrap.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+                      alt="bootstrap"
+                      width="40"
+                      height="40"
+                    />{" "}
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://www.w3schools.com/css/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
+                      alt="css3"
+                      width="40"
+                      height="40"
+                    />{" "}
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://www.typescriptlang.org/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                      alt="javascript"
+                      width="40"
+                      height="40"
+                    />{" "}
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://es.reactjs.org/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+                      alt="React"
+                      width="40"
+                      height="40"
+                    />
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://nodejs.org/en/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+                      alt="NodeJs"
+                      width="40"
+                      height="40"
+                    />
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://www.mongodb.com/cloud/atlas/lp/try4?utm_source=google&utm_campaign=search_gs_pl_evergreen_atlas_core_prosp-brand_gic-null_amers-co_ps-all_desktop_eng_lead&utm_term=mongodb&utm_medium=cpc_paid_search&utm_ad=e&utm_ad_campaign_id=12212624317&adgroup=115749712463&gclid=CjwKCAiAyfybBhBKEiwAgtB7fkGDrg7o_zrKkkkh3aeIJgFjnsVKbnI3tAS82N_kMFO-K08fxW-66xoCCnUQAvD_BwE"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+                      alt="mongoDb"
+                      width="40"
+                      height="40"
+                    />
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://www.figma.com/files/recent?fuid=1156409605228773058"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
+                      alt="figma"
+                      width="40"
+                      height="40"
+                    />{" "}
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://firebase.google.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
+                      alt="firebase"
+                      width="40"
+                      height="40"
+                    />
+                  </a>
+                </div>
+                <div className="item">
+                  <a
+                    href="https://www.python.org/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+                      alt="python"
+                      width="40"
+                      height="40"
+                    />
+                  </a>
+                </div>
+                <div className="item">
+                  <a href="https://git-scm.com/">
+                    {" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+                      alt="git"
+                      width="40"
+                      height="40"
+                    />
+                  </a>
+                </div>
+              </Carousel>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
