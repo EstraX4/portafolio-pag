9071bff7d1b24c0956c02c0d5542adb164606670
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2022-12-31 16:15:42 -0600

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,10 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
@@ -30,7 +24,7 @@ export const Skills = () => {
 
   return (
     <section className="skill" id="skills">
-      <div className="container">
+      <div className="container aligh-items-center">
         <div className="row">
           <div className="col-12">
             <div className="skill-bx wow zoomIn">
